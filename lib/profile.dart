394df6a512a3bf4b9ef6b4d4d565ef2fdf59eaad
Prakhar <prakhar2p@gmail.com>
2024-05-26 13:21:12 +0530

    feature profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -16,37 +16,39 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 19, 19, 19),
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_rounded,
-              color: Colors.white,
-              size: 30,
-            ),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => const Smshome()));
-            },
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.access_alarm_sharp,
+            color: Colors.white,
+            size: 30,
           ),
-          title: Text('Student Management System',
-              style: GoogleFonts.oswald(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold)),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Smshome()));
+          },
         ),
-        backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-        body: Column(mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                  child: Text("No profile Data",
-                      style: GoogleFonts.roboto(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )))
-            ]),
-      bottomNavigationBar: const Section4(),);
+        title: Text('Student Management System',
+            style: GoogleFonts.oswald(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold)),
+      ),
+      backgroundColor: const Color.fromARGB(255, 39, 39, 39),
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: Text("No profile Data",
+                    style: GoogleFonts.roboto(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    )))
+          ]),
+      bottomNavigationBar: const Section4(),
+    );
   }
 }
