a3effceddf08104f9719fc535856ae8e11aac6c1
Rohan <161418030+RohanXD@users.noreply.github.com>
2024-08-27 20:56:15 +0530

    Merge pull request #4 from RohanXD/version-1.04
    UI change in content Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:sms_sql/sms_home.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Smshome());
 }
